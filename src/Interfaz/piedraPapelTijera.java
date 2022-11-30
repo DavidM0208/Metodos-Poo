caa7cb9af32620882c34176cde895a31aba71b0e
DavidCM <absolpvp13@gmail.com>
2022-11-29 20:28:39 -0500

    Correción de errores

--- src/Interfaz/piedraPapelTijera.java
+++ src/Interfaz/piedraPapelTijera.java
@@ -1,7 +1,5 @@
 package Interfaz;
 
-import java.util.Random;
-
 import java.util.Scanner;
 
 public class piedraPapelTijera implements Juego {
@@ -26,31 +24,31 @@ public class piedraPapelTijera implements Juego {
         juego=(int)(Math.random()*3)+1;
 
         if(juego==1 && eleccion==1){
-            System.out.println("¡¡..EMPATE..!! \n La maquina escogio piedra, seleccionaste piedra.");
+            System.out.println("¡¡..EMPATE..!! \n La maquina escogio piedra, "+nombre+" seleciono piedra.");
         }
         else if(juego==1 && eleccion==2){
-            System.out.println("¡¡..GANASTE..!! \n La maquina escogio piedra, seleccionaste papel.");
+            System.out.println("¡¡..GANASTE..!! \n La maquina escogio piedra, "+nombre+" seleciono papel.");
         }
         else if(juego==1 && eleccion==3){
-            System.out.println("¡¡..PERDISTE..!! \n La maquina escogio piedra, seleccionaste tijera.");
+            System.out.println("¡¡..PERDISTE..!! \n La maquina escogio piedra, "+nombre+" seleciono tijera.");
         }
         else if(juego==2 && eleccion==1){
-            System.out.println("¡¡..PERDISTE..!! \n La maquina escogio papel, seleccionaste piedra.");
+            System.out.println("¡¡..PERDISTE..!! \n La maquina escogio papel, "+nombre+" seleciono piedra.");
         }
         else if(juego==2 && eleccion==2){
-            System.out.println("¡¡..EMPATE..!! \n La maquina escogio papel, seleccionaste papel.");
+            System.out.println("¡¡..EMPATE..!! \n La maquina escogio papel, "+nombre+" seleciono papel.");
         }
         else if(juego==2 && eleccion==3){
-            System.out.println("¡¡..GANASTE..!! \n La maquina escogio papel, seleccionaste tijera.");
+            System.out.println("¡¡..GANASTE..!! \n La maquina escogio papel, "+nombre+" seleciono tijera.");
         }
         else if(juego==3 && eleccion==1){
-            System.out.println("¡¡..GANASTE..!! \n La maquina escogio tijera, seleccionaste piedra.");
+            System.out.println("¡¡..GANASTE..!! \n La maquina escogio tijera, "+nombre+" seleciono piedra.");
         }
         else if(juego==3 && eleccion==2){
-            System.out.println("¡¡..Perdiste..!! \n La maquina escogio tijera, seleccionaste papel.");
+            System.out.println("¡¡..Perdiste..!! \n La maquina escogio tijera, "+nombre+" seleciono papel.");
         }
         else if(juego==3 && eleccion==3){
-            System.out.println("¡¡..EMPATE..!! \n La maquina escogio tijera, seleccionaste tijera.");
+            System.out.println("¡¡..EMPATE..!! \n La maquina escogio tijera, "+nombre+" seleciono tijera.");
         }
         else{
             System.out.println("Opcion no valida");
